9950103b5a2e2ed381f175e87c604456027a5f60
Carlos Edison Gallo <gaalloo_25c@hotmail.com>
2023-05-03 17:49:47 -0500

    cambio textos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (screen.width < 900) {
-  let tarjetas = document.getElementById("grid-tarjetas");
+  let widthContainer = document.getElementById("grid-tarjetas");
   let derecha = document.getElementById("flecha-derecha");
 
   let containercardsaprendemos = document.getElementById(
@@ -11,9 +11,74 @@ if (screen.width < 900) {
   let containercardscrecimiento2 = document.getElementById(
     "container-cards-crecimiento2"
   );
+  let next = document.getElementById("flecha-derecha");
+  let prev = document.getElementById("flecha-izquierda");
   let contadorcrecimiento = 1;
   let contadortarjetas = 1;
   let contadorAprendemos = 1;
+  let circulos = document.querySelectorAll(".circulos");
+
+  let image = widthContainer;
+  let imageContent = document.querySelectorAll(".container-tajeta");
+  let imageContentMath = imageContent.length * 100;
+  let counter = 0;
+  circulos[0].style.border = "2px solid red";
+  widthContainer.style.width = imageContentMath + "%";
+  prev.addEventListener("click", (e) => {
+    if (counter == 0) {
+      counter = imageContentMath - 100;
+      circulos[counter / 100].style.border = "2px solid red";
+      circulos[0].style.border = "none";
+      image.style.marginLeft = "-" + counter + "%";
+    } else {
+      counter = counter -= 100;
+      image.style.marginLeft = "-" + counter + "%";
+      circulos[counter / 100 + 1].style.border = "none";
+      circulos[counter / 100].style.border = "2px solid red";
+    }
+  });
+
+  next.addEventListener("click", (e) => {
+    if (counter == imageContentMath - 100) {
+      counter = 0;
+      circulos[counter / 100].style.border = "2px solid red";
+      circulos[circulos.length - 1].style.border = "none";
+      image.style.marginLeft = "-" + counter + "%";
+    } else {
+      counter = counter += 100;
+      image.style.marginLeft = "-" + counter + "%";
+      circulos[counter / 100 - 1].style.border = "none";
+      circulos[counter / 100].style.border = "2px solid red";
+    }
+  });
+  /*   console.log(circulos);
+  circulos[0].style.border = "2px solid red";
+  next.addEventListener("click", () => {
+    if (contadortarjetas == 7) {
+      contadortarjetas = 1;
+    }
+    if (contadortarjetas == 6) {
+      tarjetas.style.marginLeft = "0%";
+      circulos[circulos.length - 1].style.border = "none";
+      circulos[0].style.border = "2px solid red";
+    } else {
+      circulos[contadortarjetas - 1].style.border = "none";
+      tarjetas.style.marginLeft = "-" + contadortarjetas + "00%";
+      circulos[contadortarjetas].style.border = "2px solid red";
+    }
+
+    contadortarjetas++;
+  });
+  prev.addEventListener("click", () => {
+    if (contadortarjetas == 1) {
+      tarjetas.style.marginLeft = "-500%";
+      contadortarjetas = 5;
+    } else {
+      tarjetas.style.marginLeft = "-" + contadortarjetas + "00%";
+    }
+    console.log(contadortarjetas);
+    contadortarjetas--;
+  }); */
   /*  setInterval(() => {
     if (contadortarjetas == 7) {
       contadortarjetas = 1;
