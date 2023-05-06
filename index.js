d3554762465b09e7d3dd6db006581848e1eb08b4
Carlos Edison Gallo <gaalloo_25c@hotmail.com>
2023-05-06 17:52:46 -0500

    ultimas seccines

--- index.js
+++ index.js
@@ -107,3 +107,162 @@ if (screen.width < 900) {
     contadorcrecimiento++;
   }, 2000); */
 }
+
+let departamento = document.getElementById("departamento");
+let ciudad = document.getElementById("ciudad");
+
+let $consultar = document.getElementById("consultarCedula");
+$consultar.addEventListener("click", () => {
+  let numeroCedula = document.getElementById("cedula").value;
+
+  Swal.fire({
+    title: "Espera",
+    text: "Estamos consultando tu información",
+    imageUrl: "https://c.tenor.com/K2UGDd4acJUAAAAC/load-loading.gif",
+    imageWidth: 150,
+    imageHeight: 150,
+    showConfirmButton: false,
+    imageAlt: "Custom image",
+  });
+
+  fetch("http://localhost:3000/cliente")
+    .then((response) => response.json())
+    .then((data) => {
+      setTimeout(() => {
+        RegistroCliente(data);
+      }, 2000);
+    });
+});
+
+function RegistroCliente(data) {
+  if (data.clienteID != null) {
+    Swal.fire({
+      icon: "success",
+      title: "Registrado",
+      text: "El cliente ya se encuentra registrado",
+    });
+    let nombre = document.getElementById("nombres");
+    let apellidos = document.getElementById("apellidos");
+    let documento = document.getElementById("documento");
+    let telefono = document.getElementById("telefono");
+    let email = document.getElementById("email");
+    let direccion = document.getElementById("direccion");
+
+    nombre.value = data.clienteName;
+    apellidos.value = data.clienteApellido;
+    documento.value = data.documentoNumero;
+    telefono.value = data.clienteName;
+    email.value = data.email;
+    direccion.value = data.direccion;
+    ObtenerDepartamento(dptos, data.departamentoID, data.ciudadID);
+    if (data.tipoDocumentoID != null) {
+      let tipodocumento = document.getElementById("tipoDocumentoID");
+
+      tipodocumento.selectedIndex =
+        data.tipoDocumentoID == 7 ? 5 : data.tipoDocumentoID;
+      let requerido = document.getElementById("requerido");
+      requerido.style.display = "none";
+    } else {
+      document.getElementById("generar").disabled = true;
+      document.getElementById("editar").disabled = false;
+      document.getElementById("guardar").disabled = true;
+    }
+
+    let formulario = document.getElementById("formulario");
+    formulario.style.display = "block";
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Cliente no registrado",
+      text: "Por favor ingresa tus datos y dale guardar",
+    });
+    let tipodocumento = document.getElementById("tipoDocumentoID");
+    tipodocumento.disabled = false;
+    let formulario = document.getElementById("formulario");
+    formulario.style.display = "block";
+    document.getElementById("generar").disabled = true;
+    document.getElementById("editar").disabled = true;
+    document.getElementById("guardar").disabled = false;
+  }
+  document.addEventListener("click", (e) => {
+    if (e.target.id == "guardar") {
+      e.preventDefault();
+      let nombre = document.getElementById("nombres").value;
+      let apellidos = document.getElementById("apellidos").value;
+      let tipodocumentoID = document.getElementById("tipoDocumentoID").value;
+      let documento = document.getElementById("documento").value;
+      let telefono = document.getElementById("telefono").value;
+      let email = document.getElementById("email").value;
+      let direccion = document.getElementById("direccion").value;
+      let departamentoID;
+      let departamentoNombre = departamento.value;
+      dptos.forEach((e) => {
+        if (e.departamentoName == departamentoNombre) {
+          departamentoID = e.departamentoID;
+        }
+      });
+      /* fetch('https://panacaservices.azurewebsites.net/api/clientecrm', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body:
+                JSON.stringify({
+                    'clienteNombre': clienteNombre,
+                    'clienteApellido': clienteApellido,
+                    'documentoNumero': documentoNumero,
+                    'fechaNacimientoTimeStamp ': fechaNacimientoTimeStamp,
+                    'celularNumero': celularNumero,
+                    'email': email,
+                    'departamentoID': departamentoID,
+                    'tipoDocumentoID': tipoDocumentoID
+                })
+        })
+            .then(response => response.json())
+            .then((data) => {
+              RegistroCliente(data)
+            }); */
+    }
+    if (e.target.id == "editar") {
+      e.preventDefault();
+      let miFormulario = document.getElementById("formulario");
+
+      // Obtener todos los elementos input del formulario
+      let elementosInput = miFormulario.getElementsByTagName("input");
+      for (var i = 0; i < elementosInput.length; i++) {
+        elementosInput[i].disabled = false;
+      }
+      let tipodocumento = document.getElementById("tipoDocumentoID");
+      tipodocumento.disabled = false;
+      document.getElementById("guardar").disabled = false;
+    }
+  });
+}
+
+function ObtenerCiudades(departamentoId, ciudadID) {
+  $("ciudad").empty();
+  fetch(
+    "https://panacaservices.azurewebsites.net/api/ciudad?departamentoId=" +
+      departamentoId
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      ObtenerCiudad(data, ciudadID);
+    });
+}
+function ObtenerDepartamento(data, id, ciudadID) {
+  data.forEach((item) => {
+    if (id == item.departamentoID) {
+      departamento.value = item.departamentoName;
+      ObtenerCiudades(item.departamentoID, ciudadID);
+    }
+  });
+}
+function ObtenerCiudad(data, id) {
+  data.forEach((element) => {
+    if (element.ciudadID == id + 99) {
+      ciudad.value = element.ciudadName;
+    }
+  });
+}
